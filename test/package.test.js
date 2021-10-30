71c1256f675048426a32891ecbb4401c028627fa
Jared Hanson <jaredhanson@gmail.com>
2021-10-30 08:03:28 -0700

    Copy files from passport branch.

---
+++ test/package.test.js
@@ -0,0 +1,14 @@
+var pkg = require('..');
+
+describe('passport-multilogin', function() {
+    
+  it('should export Strategy constructor as module', function() {
+    expect(pkg).to.be.a('function');
+    expect(pkg).to.equal(pkg.Strategy);
+  });
+  
+  it('should export Strategy constructor', function() {
+    expect(pkg.Strategy).to.be.a('function');
+  });
+  
+});
