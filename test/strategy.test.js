04af47f18cc5d61012213bce19e7d30138cf88cf
Jared Hanson <jaredhanson@gmail.com>
2021-11-01 07:43:56 -0700

    Clean up tests.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -129,66 +129,46 @@ describe('MultiSessionStrategy', function() {
       .authenticate();
   });
   
-  describe('handling a request with multiple login sessions and multi option', function() {
+  it('handling a request with multiple login sessions and multi option', function(done) {
     var strategy = new MultiSessionStrategy(function(user, req, done) {
       done(null, user);
     });
     
-    var request, pass = false;
-  
-    before(function(done) {
-      chai.passport.use(strategy)
-        .pass(function() {
-          pass = true;
-          done();
-        })
-        .request(function(req) {
-          request = req;
-          
-          req._passport = {};
-          req._passport.instance = {};
-          req.session = {};
-          req.session['passport'] = {};
-          req.session['passport'][0] = {};
-          req.session['passport'][0].user = { id: '123456', displayName: 'Alice' };
-          req.session['passport'][1] = {};
-          req.session['passport'][1].user = { id: '123457', displayName: 'Bob' };
-        })
-        .authenticate({ multi: true });
-    });
-  
-    it('should pass', function() {
-      expect(pass).to.be.true;
-    });
-    
-    it('should set user on request', function() {
-      expect(request.user).to.deep.equal([ {
-        id: '123456',
-        displayName: 'Alice'
-      }, {
-        id: '123457',
-        displayName: 'Bob'
-      } ]);
-    });
-    
-    it('should set authInfo on request', function() {
-      expect(request.authInfo).to.deep.equal([ {
-        sessionSelector: '0'
-      }, {
-        sessionSelector: '1'
-      } ]);
-    });
-    
-    it('should maintain session', function() {
-      expect(request.session['passport']).to.deep.equal({
-        0: {
-          user: { id: '123456', displayName: 'Alice' }
-        },
-        1: {
-          user: { id: '123457', displayName: 'Bob' }
-        }
-      });
-    });
+    chai.passport.use(strategy)
+      .pass(function() {
+        expect(this.user).to.deep.equal([ {
+          id: '123456',
+          displayName: 'Alice'
+        }, {
+          id: '123457',
+          displayName: 'Bob'
+        } ]);
+        expect(this.authInfo).to.deep.equal([ {
+          sessionSelector: '0'
+        }, {
+          sessionSelector: '1'
+        } ]);
+        expect(this.session['passport']).to.deep.equal({
+          0: {
+            user: { id: '123456', displayName: 'Alice' }
+          },
+          1: {
+            user: { id: '123457', displayName: 'Bob' }
+          }
+        });
+        done();
+      })
+      .request(function(req) {
+        req._passport = {};
+        req._passport.instance = {};
+        req.session = {};
+        req.session['passport'] = {};
+        req.session['passport'][0] = {};
+        req.session['passport'][0].user = { id: '123456', displayName: 'Alice' };
+        req.session['passport'][1] = {};
+        req.session['passport'][1].user = { id: '123457', displayName: 'Bob' };
+      })
+      .authenticate({ multi: true });
   });
   
 });
