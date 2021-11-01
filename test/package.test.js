a5517130ad3f7e3477c3bb8d3944958c4ec3808a
Jared Hanson <jaredhanson@gmail.com>
2021-11-01 07:00:32 -0700

    Export SessionStrategy alias.

--- test/package.test.js
+++ test/package.test.js
@@ -9,6 +9,12 @@ describe('passport-multilogin', function() {
   
   it('should export Strategy constructor', function() {
     expect(pkg.Strategy).to.be.a('function');
+    expect(pkg.SessionStrategy).to.be.a('function');
+    expect(pkg.Strategy).to.equal(pkg.SessionStrategy);
+  });
+  
+  it('should export SessionMananger constructor', function() {
+    expect(pkg.SessionManager).to.be.a('function');
   });
   
 });
