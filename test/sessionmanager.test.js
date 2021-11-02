7a3e8f3d319350202ee6df14c33a3629a16ea6da
Jared Hanson <jaredhanson@gmail.com>
2021-11-01 21:08:49 -0700

    Add tests.

--- test/sessionmanager.test.js
+++ test/sessionmanager.test.js
@@ -3,6 +3,7 @@
 
 var expect = require('chai').expect;
 var chai = require('chai');
+var sinon = require('sinon');
 var SessionManager = require('../lib/sessionmanager');
 
 
@@ -11,7 +12,8 @@ describe('Strategy', function() {
   describe('#logIn', function() {
     
     it('should establish initial session', function(done) {
-      var manager = new SessionManager(function(user, req, cb) {
+      var genh = sinon.stub().returns('a001');
+      var manager = new SessionManager({ genh: genh }, function(user, req, cb) {
         cb(null, user);
       });
     
@@ -27,11 +29,11 @@ describe('Strategy', function() {
       };
     
       manager.logIn(req, user, info, function(err) {
-        var timestamp = req.session['passport'][0].events[0].timestamp;
-        delete req.session['passport'][0].events[0].timestamp; 
+        var timestamp = req.session['passport']['a001'].events[0].timestamp;
+        delete req.session['passport']['a001'].events[0].timestamp;
         
         expect(req.session['passport']).to.deep.equal({
-          0: {
+          'a001': {
             user: { id: '248289761001', displayName: 'Jane Doe' },
             events: [ {
               method: 'password'
@@ -41,7 +43,56 @@ describe('Strategy', function() {
         expect(timestamp).to.be.closeToTime(new Date(), 1);
         done();
       })
-    });
-  });
+    }); // should establish initial session
+    
+    it('should establish second session', function(done) {
+      var genh = sinon.stub().returns('a002');
+      var manager = new SessionManager({ genh: genh }, function(user, req, cb) {
+        cb(null, user);
+      });
+    
+      var req = new Object();
+      req.session = {};
+      req.session['passport'] = {
+        'a001': {
+          user: { id: '248289761001', displayName: 'Jane Doe' },
+          events: [ {
+            method: 'password'
+          } ]
+        }
+      };
+      
+      var user = {
+        id: '248289761002',
+        displayName: 'John Doe'
+      };
+      var info = {
+        method: 'password'
+      };
+    
+      manager.logIn(req, user, info, function(err) {
+        var timestamp = req.session['passport']['a002'].events[0].timestamp;
+        delete req.session['passport']['a002'].events[0].timestamp;
+        
+        expect(req.session['passport']).to.deep.equal({
+          'a001': {
+            user: { id: '248289761001', displayName: 'Jane Doe' },
+            events: [ {
+              method: 'password'
+            } ]
+          },
+          'a002': {
+            user: { id: '248289761002', displayName: 'John Doe' },
+            events: [ {
+              method: 'password'
+            } ]
+          }
+        });
+        expect(timestamp).to.be.closeToTime(new Date(), 1);
+        done();
+      })
+    }); // should establish second session
+    
+  }); // #logIn
   
 });
