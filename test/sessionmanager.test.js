d6ca3b39ea718d847d724ddcaea6160d9782f43e
Jared Hanson <jaredhanson@gmail.com>
2021-11-01 21:28:32 -0700

    Add re-auth behavior and tests.

--- test/sessionmanager.test.js
+++ test/sessionmanager.test.js
@@ -45,6 +45,49 @@ describe('Strategy', function() {
       })
     }); // should establish initial session
     
+    it('should update initial session on reauthentication with password', function(done) {
+      var genh = sinon.stub().returns('a001');
+      var manager = new SessionManager({ genh: genh }, function(user, req, cb) {
+        cb(null, user);
+      });
+    
+      var req = new Object();
+      req.session = {};
+      req.session['passport'] = {
+        'a001': {
+          user: { id: '248289761001', displayName: 'Jane Doe' },
+          events: [ {
+            method: 'password',
+            timestamp: new Date(Date.now() - 86400)
+          } ]
+        }
+      };
+      
+      var user = {
+        id: '248289761001',
+        displayName: 'Jane Doe'
+      };
+      var info = {
+        method: 'password'
+      };
+    
+      manager.logIn(req, user, info, function(err) {
+        var timestamp = req.session['passport']['a001'].events[0].timestamp;
+        delete req.session['passport']['a001'].events[0].timestamp;
+        
+        expect(req.session['passport']).to.deep.equal({
+          'a001': {
+            user: { id: '248289761001', displayName: 'Jane Doe' },
+            events: [ {
+              method: 'password'
+            } ]
+          }
+        });
+        expect(timestamp).to.be.closeToTime(new Date(), 1);
+        done();
+      })
+    }); // should update initial session on reauthentication with password
+    
     it('should establish second session', function(done) {
       var genh = sinon.stub().returns('a002');
       var manager = new SessionManager({ genh: genh }, function(user, req, cb) {
