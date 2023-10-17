0c14ab3b0f5d63562a69a6f8944331e00c466a75
Jared Hanson <jaredhanson@gmail.com>
2023-10-17 14:32:27 -0700

    Invalid sessions on multi option.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -739,6 +739,66 @@ describe('Strategy', function() {
       .authenticate();
   }); // should remove secondary session when login session has been invalidated
   
+  it('should remove secondary session when login session has been invalidated using multi option', function(done) {
+    var verify = sinon.stub();
+    verify.onCall(0).yields(null, { id: '248289761001', displayName: 'Jane Doe' });
+    verify.onCall(1).yields(null, false);
+    var strategy = new Strategy(verify);
+    
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.session = {};
+        req.session['passport'] = {
+          default: 'a001',
+          sessions: {
+            'a001': {
+              user: { id: '248289761001', displayName: 'Jane Doe' },
+              methods: [ {
+                type: 'password',
+                timestamp: new Date(Date.now() - 7200000)
+              } ]
+            },
+            'a002': {
+              user: { id: '248289761002', displayName: 'John Doe' },
+              methods: [ {
+                type: 'password',
+                timestamp: new Date(Date.now() - 3600000)
+              } ]
+            }
+          }
+        };
+      })
+      .pass(function() {
+        expect(this.user).to.deep.equal({
+          id: '248289761001',
+          displayName: 'Jane Doe'
+        });
+        expect(this.authInfo).to.deep.equal({
+          methods: [ {
+            type: 'password',
+            timestamp: new Date('2011-07-21T18:42:50.000Z')
+          } ],
+          sessionSelector: 'a001'
+        });
+        expect(this.session).to.deep.equal({
+          passport: {
+            default: 'a001',
+            sessions: {
+              'a001': {
+                user: { id: '248289761001', displayName: 'Jane Doe' },
+                methods: [ {
+                  type: 'password',
+                  timestamp: new Date('2011-07-21T18:42:50.000Z')
+                } ]
+              }
+            }
+          }
+        });
+        done();
+      })
+      .authenticate({ multi: true });
+  }); // should remove secondary session when login session has been invalidated
+  
   it('should error when session is not available', function(done) {
     var strategy = new Strategy();
     
