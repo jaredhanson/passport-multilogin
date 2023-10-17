50c019202bec555b42a484c38a6d68796e25efb4
Jared Hanson <jaredhanson@gmail.com>
2023-10-17 11:46:33 -0700

    Add tests>

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -782,4 +782,40 @@ describe('Strategy', function() {
       .authenticate();
   }); // should error when verify function encounters an error
   
+  it('should error when verify function encounters an error using multi option', function(done) {
+    var strategy = new Strategy(function(req, user, cb) {
+      cb(new Error('something went wrong'));
+    });
+    
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.session = {};
+        req.session['passport'] = {
+          default: 'a001',
+          sessions: {
+            'a001': {
+              user: { id: '248289761001', displayName: 'Jane Doe' },
+              methods: [ {
+                type: 'password',
+                timestamp: new Date(Date.now() - 7200000)
+              } ]
+            },
+            'a002': {
+              user: { id: '248289761002', displayName: 'John Doe' },
+              methods: [ {
+                type: 'password',
+                timestamp: new Date(Date.now() - 3600000)
+              } ]
+            }
+          }
+        };
+      })
+      .error(function(err) {
+        expect(err).to.be.an.instanceOf(Error);
+        expect(err.message).to.equal("something went wrong");
+        done();
+      })
+      .authenticate({ multi: true });
+  }); // should error when verify function encounters an error using multi option
+  
 });
